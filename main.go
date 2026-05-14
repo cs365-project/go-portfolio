0ad6e47e54ca626c50debdfd0410f3dea2959f14
kittiphonkamnuan12-jpg <kittiphonkamnuan12@gmail.com>
2026-05-14 11:45:46 +0700

    demo:v5

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	// ชี้ไปที่โฟลเดอร์ static เพื่อเสิร์ฟไฟล์ HTML
-	fs := http.FileServer(http.Dir("./e2e"))
+	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
 	fmt.Println("Server is running on port 8080...")
 	// รันเซิร์ฟเวอร์ที่ Port 8080
-	log.Fatal(http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
